7cca0104c4cb4633551c5f5896a39aa725684535
guskri00 <guskri00@gmail.com>
2024-02-19 17:48:54 +0100

    test blinker to check if base program works

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/*  Blink
 
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+// the setup function runs once when you press reset or power the board
+*/
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
